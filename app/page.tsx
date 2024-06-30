6fcb18c64435f535a87d1d5dbd686cc5e420965f
syedshahmirsultan <syedshahmirsultan@gmail.com>
2024-06-30 20:30:14 +0500

    added RecordingView.tsx file code

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import MicrophoneComponent from './components/RecordingView';
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className='flex items-center justify-center bg-gray-100 '>
+      <MicrophoneComponent/>
     </div>
   );
 }
